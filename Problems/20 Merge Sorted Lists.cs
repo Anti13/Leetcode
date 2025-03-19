9375962c5e80780444aca34440241651914bada0
Anti13 <38919928+Anti13@users.noreply.github.com>
2025-03-19 07:57:04 +0100

    improved performance

--- Problems/20 Merge Sorted Lists.cs
+++ Problems/20 Merge Sorted Lists.cs
@@ -11,78 +11,44 @@
  */
 public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 ==null && list2==null)
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        var dummy = new ListNode();
+        var current = dummy;
+
+        while(true)
         {
-            return null;
+            if(list1 == null || list2 == null)
+            {
+                break;
+            }
+
+            if(list1.val < list2.val)
+            {
+                current.next= list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
         }
 
-                if(list1 ==null && list2!=null)
+        if(list1 !=null)
         {
-            return list2;
+            current.next = list1;
         }
 
-                if(list1 !=null && list2==null)
+                if(list2 !=null)
         {
-            return list1;
+            current.next = list2;
         }
+        
+        return dummy.next;
 
-        ListNode result =  null;
-        var current = result;
-        if(list1.val>list2.val)
-        {
-            var temp =new ListNode(list2.val,null);
-            result = temp;
-            list2 = list2.next;
-        }
-        else
-        {
-            var temp =new ListNode(list1.val,null);
-            result = temp;
-              list1 = list1.next;
-        }
-        current = result;
-
-                while(true)
-        {
-        if(list1 ==null && list2==null)
-        {
-            break;
-        }
-
-                if(list1 ==null && list2!=null)
-        {
-            var temp =new ListNode(list2.val,null);
-            current.next = temp;
-             current = temp;
-             list2=list2.next;
-            continue;
-
-        }
-                if(list1 !=null && list2==null)
-        {
-            var temp =new ListNode(list1.val,null);
-           current.next = temp;
-             current = temp;
-             list1=list1.next;
-             continue;
-        }
-
-        if(list1.val < list2.val)
-        {
-            var temp =new ListNode(list1.val,null);
-          current.next = temp;
-             current = temp;
-             list1=list1.next;
-        }
-        else
-        {
-            var temp =new ListNode(list2.val,null);
-         current.next = temp;
-             current = temp;
-             list2=list2.next;
-        }
-        }
-
-    return result;
     }
 }

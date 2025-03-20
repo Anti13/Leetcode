765766b1dc5789a87ac53104fbcd5c4716b96216
Anti13 <38919928+Anti13@users.noreply.github.com>
2025-03-20 17:41:17 +0100

    optimized version

--- Problems/283 Move Zeroes.cs
+++ Problems/283 Move Zeroes.cs
@@ -1,51 +1,17 @@
 
 public class Solution {
     public void MoveZeroes(int[] nums) {
-        int len = nums.Length;
+        int l=0;
 
-        int firstZero =-1;
-        int firstNonZero =-1;
-
-
-
-        while(true)
+        for(int r =0;r<nums.Length;r++)
         {
-                    firstZero=FindNext(firstZero, nums, len, true);
-        firstNonZero=FindNext(firstZero, nums, len, false);
-
-           if(firstNonZero == len || firstZero == len)
-           {
-            return;
-           }
-
-           var temp = nums[firstZero];
-            nums[firstZero] =  nums[firstNonZero];
-            nums[firstNonZero] = temp;
-
-
-        }
-        
-    }
-
-    private int FindNext(int index, int[] nums ,int len, bool wantZero)
-    {        
-        while(true)
-        {
-                        index++;
-
-            if(index > len-1)
+            if(nums[r] !=0)
             {
-                return index;
+                var temp = nums[r];
+                nums[r]= nums[l];
+                nums[l] = temp;
+                l++;
             }
-
-            if(wantZero && nums[index] ==0)
-            {
-                return index;
-            }
-            else  if(!wantZero && nums[index] !=0)
-            {
-                return index;
-            }
-        }
+        }   
     }
 }
